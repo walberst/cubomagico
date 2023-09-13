848db337f6743b6e76722cb35fe13a7e95e299e8
Antonio Walber <antoniowalber3000@hotmail.com.br>
2023-09-13 18:03:00 -0300

    definido estilo do cubo

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let box = document.querySelector(".box");
+
+const color = [
+  "#3db911",
+  "#df980b",
+  "#04a4f4",
+  "#04f4a8",
+  "#1932f3",
+  "#bd0049",
+];
+
+const directions = ["front", "back", "left", "right", "top", "bottom"];
+
+for (var i of directions) {
+  let direction = document.createElement("div");
+  box.appendChild(direction);
+  direction.classList.add(i);
+  direction.setAttribute("style", `--clr:${color[directions.indexOf(i)]}`);
+}
