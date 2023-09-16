775261b477f4d1cd06271181f9505a3ffa597706
Antonio Walber <antoniowalber3000@hotmail.com.br>
2023-09-16 01:10:19 -0300

    scoreboard e correção de alguns bugs

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ const box = document.querySelector(".box");
 
 const directions = ["front", "back", "left", "right", "top", "bottom"];
 
+var movementsPoints = 0;
+
 // Create direction elements and append them to the container
 function createDirection(directionName) {
   const direction = document.createElement("div");
@@ -48,7 +50,9 @@ function createDirection(directionName) {
 
     const configs = buttonConfig[j];
     for (var config of configs) {
-      span.appendChild(buttonsRotateBlock(config.side, config.column));
+      span.appendChild(
+        buttonsRotateBlock(directionName, config.side, config.column)
+      );
     }
   }
 
@@ -94,12 +98,8 @@ function rotateCube(x, y) {
   box.style.transform = `rotateX(${Yvalue}deg) rotateY(${Xvalue}deg)`;
 }
 
-// Function to rotate blocks
-function rotateBlock(side, direction, column) {
-  direction = direction === "vertical" ? "v" : "h";
-
+function getPositionsCube() {
   const faces = ["front", "back", "left", "right", "top", "bottom"];
-
   const cube = {};
 
   for (const face of faces) {
@@ -122,54 +122,86 @@ function rotateBlock(side, direction, column) {
     }
   }
 
-  if (direction === "v" && side === "up") {
-    const temp = JSON.parse(
-      JSON.stringify([
-        cube["front"][`${direction}${column}`][0].className,
-        cube["front"][`${direction}${column}`][1].className,
-        cube["front"][`${direction}${column}`][2].className,
-      ])
-    );
-    const facesOrder = ["front", "bottom", "back", "top"];
-    for (let i = 0; i < facesOrder.length; i++) {
-      const currentFace = facesOrder[i];
-      const nextFace = facesOrder[(i + 1) % facesOrder.length];
-      var x = 0;
-      for (var sub of cube[currentFace][`${direction}${column}`]) {
-        sub.className = cube[nextFace][`${direction}${column}`][0].className;
-        if (currentFace === "top") {
-          sub.className = temp[x];
-        }
-        x++;
+  return cube;
+}
+
+// Function to rotate blocks
+function rotateBlock(face, side, verticality, column) {
+  const direction = verticality === "vertical" ? "v" : "h";
+
+  const cube = getPositionsCube();
+
+  const movements = {
+    vertical: {
+      front: {
+        up: ["front", "bottom", "back", "top"],
+        down: ["front", "top", "back", "bottom"],
+      },
+      back: {
+        up: ["back", "top", "front", "bottom"],
+        down: ["back", "bottom", "top", "front"],
+      },
+      left: {
+        up: ["left", "bottom", "right", "top"],
+        down: ["left", "top", "bottom", "right"],
+      },
+      right: {
+        up: ["right", "bottom", "left", "top"],
+        down: ["right", "top", "bottom", "left"],
+      },
+      top: {
+        up: ["top", "back", "bottom", "front"],
+        down: ["top", "front", "bottom", "back"],
+      },
+      bottom: {
+        up: ["bottom", "front", "top", "back"],
+        down: ["bottom", "back", "top", "front"],
+      },
+    },
+    horizontal: {
+      front: {
+        left: ["front", "right", "back", "left"],
+        right: ["front", "left", "back", "right"],
+      },
+      back: {
+        left: ["back", "right", "front", "left"],
+        right: ["back", "left", "front", "right"],
+      },
+      left: {
+        left: ["left", "front", "right", "back"],
+        right: ["left", "back", "right", "front"],
+      },
+      right: {
+        left: ["right", "back", "left", "front"],
+        right: ["right", "front", "left", "back"],
+      },
+      top: {
+        left: ["top", "right", "bottom", "left"],
+        right: ["top", "left", "bottom", "right"],
+      },
+      bottom: {
+        left: ["bottom", "right", "bottom", "left"],
+        right: ["bottom", "left", "bottom", "right"],
+      },
+    },
+  };
+
+  const temp = cube[face][`${direction}${column}`].map((sub) => sub.className);
+  const facesOrder = movements[verticality][face][side];
+
+  facesOrder.forEach((currentFace, i) => {
+    const nextFace = facesOrder[(i + 1) % facesOrder.length];
+    cube[currentFace][`${direction}${column}`].forEach((sub, j) => {
+      sub.className = cube[nextFace][`${direction}${column}`][0].className;
+      if (currentFace === facesOrder[facesOrder.length - 1]) {
+        sub.className = temp[j];
       }
-    }
-  }
-  if (direction === "v" && side === "down") {
-    const temp = JSON.parse(
-      JSON.stringify([
-        cube["front"][`${direction}${column}`][0].className,
-        cube["front"][`${direction}${column}`][1].className,
-        cube["front"][`${direction}${column}`][2].className,
-      ])
-    );
-    const facesOrder = ["front", "top", "back", "bottom"];
-    for (let i = 0; i < facesOrder.length; i++) {
-      const currentFace = facesOrder[i];
-      const nextFace = facesOrder[(i + 1) % facesOrder.length];
-      var x = 0;
-      for (var sub of cube[currentFace][`${direction}${column}`]) {
-        sub.className = cube[nextFace][`${direction}${column}`][0].className;
-        if (currentFace === "top") {
-          sub.className = temp[x];
-        }
-        x++;
-      }
-    }
-  }
+    });
+  });
 }
 
 // Function to make buttons to rotate blocks
-function buttonsRotateBlock(side, column) {
+function buttonsRotateBlock(face, side, column) {
   if (side === "") {
     let img = document.createElement("img");
     img.src = "img/logo.jpg";
@@ -185,11 +217,32 @@ function buttonsRotateBlock(side, column) {
     if (side === "left" || side === "right") {
       direction = "horizontal";
     }
-    rotateBlock(side, direction, column);
+    rotateBlock(face, side, direction, column);
+    scoreboard();
   });
 
   button.classList.add("rotate_block");
   icon.classList.add("fas", `fa-arrow-${side}`);
 
   return button;
+}
+
+function scoreboard() {
+  movementsPoints++;
+  document.getElementById("movements").innerHTML = movementsPoints;
+
+  let fullSides = 0;
+  fullSides +=
+    document.querySelectorAll(".front span.cube_front").length === 9 ? 1 : 0;
+  fullSides +=
+    document.querySelectorAll(".bottom span.cube_bottom").length === 9 ? 1 : 0;
+  fullSides +=
+    document.querySelectorAll(".back span.cube_back").length === 9 ? 1 : 0;
+  fullSides +=
+    document.querySelectorAll(".top span.cube_top").length === 9 ? 1 : 0;
+  fullSides +=
+    document.querySelectorAll(".left span.cube_left").length === 9 ? 1 : 0;
+  fullSides +=
+    document.querySelectorAll(".right span.cube_right").length === 9 ? 1 : 0;
+  document.getElementById("fullSides").innerHTML = `${fullSides} / 6`;
 }
